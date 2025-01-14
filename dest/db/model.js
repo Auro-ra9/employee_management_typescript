7dfc7d50c8d66b8fc093b0636a1b28ac36825cf1
Auro-ra9 <ashnaahammed9@gmail.com>
2025-01-15 01:11:26 +0530

    debugged cache error

--- dest/db/model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// interface userDetails{
-//     name:string;
-//     age:number;
-// }
-// type orderStatus="pending"| "delivered" |"confirmed"
